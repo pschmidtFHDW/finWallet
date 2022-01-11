b299c2da23fd9e5a4ea7c9765f0562e067e33ce2
pschmidtFHDW <78427385+pschmidtFHDW@users.noreply.github.com>
2022-01-11 23:16:11 +0100

    Initial Project Creation

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'finwallet-7fee2',
+    appId: '1:307588586390:web:ffb4131b17079abeeadbb7',
+    storageBucket: 'finwallet-7fee2.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDqmYGD1tVWjqQeDBDpLi5zGLxWoYnjiyE',
+    authDomain: 'finwallet-7fee2.firebaseapp.com',
+    messagingSenderId: '307588586390',
+    measurementId: 'G-MLPH8JRCM8',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'finwallet-7fee2',
+    appId: '1:307588586390:web:ffb4131b17079abeeadbb7',
+    storageBucket: 'finwallet-7fee2.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDqmYGD1tVWjqQeDBDpLi5zGLxWoYnjiyE',
+    authDomain: 'finwallet-7fee2.firebaseapp.com',
+    messagingSenderId: '307588586390',
+    measurementId: 'G-MLPH8JRCM8',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
